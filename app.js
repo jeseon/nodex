70027fb43b5815e758c91508f674c5ecf761ff25
jeseon <jeseon@gmail.com>
2016-03-29 21:43:17 +0900

    7-3. 환경변수(environment variables) 설정

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 var express = require('express'),
+    mongoose = require('mongoose'),
     path = require('path'),
     app = express();
+
+mongoose.connect(process.env.MONGODB)
+
+var db = mongoose.connection;
+db.once('open', function() {
+    console.log('DB connected!');
+});
+
+db.on('error', function(err) {
+    console.log('DB ERROR: ', err);
+});
+
+var dataSchema = mongoose.Schema({
+        name: String,
+        count: Number
+    }),
+    Data = mongoose.model('data', dataSchema);
+
+Data.findOne({name: 'myData'}, function(err, data) {
+    if (err) return console.log('Data ERROR:' + err);
+    else {
+        if (!data) {
+            Data.create({name: 'myData', count:0}, function(err, data) {
+                if (err) return console.log('Data ERROR:', err);
+                else console.log('Counter initialized:', data);
+            });
+        }
+    }
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,26 +38,55 @@ app.use(express.static(path.join(__dirname, 'public')));
 var data = { count: 0 };
 
 app.get('/', function(req, res) {
-    data.count++;
-    res.render('index', data);
+    Data.findOne({name: 'myData'}, function(err, data) {
+        if (err) return console.log('Data ERROR:' + err);
+        else {
+            data.count++;
+            data.save(function(err) {
+                if (err) return console.log('Data ERROR: ', err);
+                else res.render('index', data);
+            });
+        }
+    });
 });
 
 app.get('/reset', function(req, res) {
-    data.count = 0;
-    res.render('index', data);
+    setCounter(res, 0);
 });
 
 app.get('/count/set', function(req, res) {
-    if (req.query.num)
-        data.count = req.query.num;
-    res.render('index', data);
+    if (req.query.num) setCounter(res, req.query.num);
+    else getCounter(res);
 });
 
 app.get('/count/set/:num', function(req, res) {
-    data.count = req.params.num;
-    res.render('index', data);
+    if (req.params.num) setCounter(res, req.params.num);
+    else getCounter(res);
 });
 
 app.listen(3000, function() {
     console.log('Server On!');
 });
+
+
+function setCounter(res, num) {
+    console.log('setCounter');
+    Data.findOne({name: 'myData'}, function(err, data) {
+        if (err) return console.log('Data ERROR:' + err);
+        else {
+            data.count = num;
+            data.save(function(err) {
+                if (err) return console.log('Data ERROR: ', err);
+                else res.render('index', data);
+            });
+        }
+    });
+}
+
+function getCounter(res) {
+    console.log('getCounter');
+    Data.findOne({name: 'myData'}, function(err, data) {
+        if (err) return console.log('Data ERROR:' + err);
+        else res.render('index', data);
+    });
+}
